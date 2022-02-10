86c7a036775748419b1237607fd7f2416ea1e578
hugo <hugor@ifit.com>
2022-02-10 14:31:33 -0500

    add profile button in nav bar

--- iFIT.Mobile.Profile.Droid/MainActivity.cs
+++ iFIT.Mobile.Profile.Droid/MainActivity.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using Android.App;
+﻿using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using AndroidX.AppCompat.Widget;
 using AndroidX.AppCompat.App;
-using Google.Android.Material.FloatingActionButton;
-using Google.Android.Material.Snackbar;
+
 
 namespace iFIT.Mobile.Profile.Droid
 {
@@ -22,9 +20,7 @@ namespace iFIT.Mobile.Profile.Droid
 
             Toolbar toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
+            
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -38,22 +34,14 @@ namespace iFIT.Mobile.Profile.Droid
             int id = item.ItemId;
             if (id == Resource.Id.action_settings)
             {
+                Intent intent = new Intent(this.ApplicationContext, typeof(ProfileActivity));
+                StartActivity(intent);
                 return true;
             }
 
             return base.OnOptionsItemSelected(item);
         }
-
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (View.IOnClickListener)null).Show();
-
-            Intent intent = new Intent(this.ApplicationContext, typeof(ProfileActivity));
-            StartActivity(intent);
-        }
-
+        
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
