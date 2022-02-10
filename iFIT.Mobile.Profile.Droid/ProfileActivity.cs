d9be37327307094f8c549f3694ae5a773464bbad
Jordan Brownfield <jordanb@ifit.com>
2022-02-10 11:42:16 -0700

    move text to xml

--- iFIT.Mobile.Profile.Droid/ProfileActivity.cs
+++ iFIT.Mobile.Profile.Droid/ProfileActivity.cs
@@ -32,9 +32,6 @@ namespace iFIT.Mobile.Profile.Droid
             MaterialButton followButton = FindViewById<MaterialButton>(Resource.Id.follow_button);
             followButton.Text = "Follow";
 
-            TextView clendarItemTitle = FindViewById<TextView>(Resource.Id.title);
-            clendarItemTitle.Text = "Kilauea Climb Interval Ride";
-
             var clock = FindViewById<ImageView>(Resource.Id.calorie);
             clock.SetColorFilter(Color.Argb (100, 46, 49, 52), PorterDuff.Mode.SrcIn);
             var timer = FindViewById<ImageView>(Resource.Id.timer);
@@ -45,27 +42,6 @@ namespace iFIT.Mobile.Profile.Droid
             elevation.SetColorFilter(Color.Argb (100, 46, 49, 52), PorterDuff.Mode.SrcIn);
             var check = FindViewById<ImageView>(Resource.Id.check);
             check.SetColorFilter(Color.Argb (100, 46, 49, 52), PorterDuff.Mode.SrcIn);
-            
-            var type = FindViewById<TextView>(Resource.Id.type);
-            type.Text = "Cycling";
-
-            var calendarItemStat1 = FindViewById<TextView>(Resource.Id.calendarStat1);
-            calendarItemStat1.Text = "34:22";
-            
-            var calendarItemStat2 = FindViewById<TextView>(Resource.Id.calendarStat2);
-            calendarItemStat2.Text = "224 CALS";
-            
-            var calendarItemStat3 = FindViewById<TextView>(Resource.Id.calendarStat3);
-            calendarItemStat3.Text = "5.68 MI";
-            
-            var calendarItemStat4 = FindViewById<TextView>(Resource.Id.calendarStat4);
-            calendarItemStat4.Text = "126 FT";
-            
-            var time = FindViewById<TextView>(Resource.Id.time);
-            time.Text = "16:22";
-
-            var detailsBtn = FindViewById<Button>(Resource.Id.details);
-            detailsBtn.Text = "View Details ->";
         }
     }
 }
