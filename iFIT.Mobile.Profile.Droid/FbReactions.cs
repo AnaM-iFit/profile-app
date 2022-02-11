65f90498510c4fa9be8f2b680fe3586fba7578a0
Anastasia.Mizumskaya <anastasia.miziumska@trinetix.com>
2022-02-11 19:35:24 +0200

    wip

--- iFIT.Mobile.Profile.Droid/FbReactions.cs
+++ iFIT.Mobile.Profile.Droid/FbReactions.cs
@@ -15,8 +15,8 @@ namespace iFIT.Mobile.Profile.Droid
             new Reaction(ReactConstants.LOVE, ReactConstants.RED_LOVE, Resource.Drawable.ic_heart),
             new Reaction(ReactConstants.SMILE, ReactConstants.YELLOW_WOW, Resource.Drawable.ic_happy),
             new Reaction(ReactConstants.WOW, ReactConstants.YELLOW_WOW, Resource.Drawable.ic_surprise),
-            new Reaction(ReactConstants.SAD, ReactConstants.YELLOW_HAHA, Resource.Drawable.ic_sad),
-            new Reaction(ReactConstants.ANGRY, ReactConstants.RED_ANGRY, Resource.Drawable.ic_angry),
+            // new Reaction(ReactConstants.SAD, ReactConstants.YELLOW_HAHA, Resource.Drawable.ic_sad),
+            // new Reaction(ReactConstants.ANGRY, ReactConstants.RED_ANGRY, Resource.Drawable.ic_angry),
         };
     }
 }
